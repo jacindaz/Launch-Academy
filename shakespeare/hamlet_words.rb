a3ff3f03ee0657b604e47da68e67c72ccdbdc525
Jacinda Zhong <jacindaz@gmail.com>
2014-05-15 12:16:28 -0400

    Added rio gem to read up to a specified line
     - reads up to a specified line in a specified file
     - uses the rio gem to read line by line
     - returns a hash with each word and the number of times each word appears in the text

--- shakespeare/hamlet_words.rb
+++ shakespeare/hamlet_words.rb
@@ -1,26 +1,35 @@
-#file = File.read("hamlet.txt")
-#read_file = IO.readlines("hamlet.txt")
-#puts read_file[0]
-#puts read_file[1]
 require 'pry'
 require 'rio'
 
-def read_file(file_name)
+#ABOUT THE WORD_COUNT METHOD---------------------------------
+#read a certain file until a specified line number
+#then return a hash with the count of each word
+
+def word_count(file_name, line)
   #file_array = []
   count_words_hash = {}
-  count_lines = 0
+  line_index = 0
 
-  IO.foreach(file_name) do |line|
-    puts "Line: #{count_lines}, text: #{line}"
-      file_array = line.split(" ")
-      file_array.each do |word|
-        if !count_words_hash.has_key?(word)
-          count_words_hash[word] = 0
-        end
-        count_words_hash[word] += 1
-      end #end array each loop
-  end #end file read each line loop
+  while line_index <= line
+    line_text = rio("hamlet.txt").lines[line_index]
+    line_array_index0 = line_text[0]
+    words_array = line_array_index0.split(" ")
 
-  puts "Final hash: #{count_words_hash}, count: #{count_lines}"
-end
-read_file("hamlet.txt")
+    #puts "Line array string: #{line_array_index0}"
+    words_array.each do |word|
+      #puts "Line text: #{line_text}, word: #{word}, index: #{line_index}"
+      if !count_words_hash.has_key?(word)
+        count_words_hash[word] = 0
+      end
+      count_words_hash[word] += 1
+    end #end array each loop
+    line_index += 1
+  end #end while loop
+  puts "Final hash: #{count_words_hash}"
+
+  return count_words_hash
+
+end #end read_file method
+
+
+word_count("hamlet.txt", 3)
