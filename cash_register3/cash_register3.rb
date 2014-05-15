cb3fef91263d4d91cccb2ebc5b39a61f33986481
Jacinda Zhong <jacindaz@gmail.com>
2014-05-15 15:09:39 -0400

    Corrected displayTransactionOptions method, created global hash
    display transaction options method:
     1) updated this method to access the new global hash
     2) displays retail price (with 2 decimal places), with name of item
     3) puts a list of selection options for the user
    
    global hash
     1) creating global hash made the code more DRY
     2) only need to call the read_file method once, instead of recreating
        the has everytime needed to use the hash

--- cash_register3/cash_register3.rb
+++ cash_register3/cash_register3.rb
@@ -1,39 +1,37 @@
 require 'csv'
 
+
 #read_file reads a CSV file, returns has with SKU, item name, and retail price ($$)----------------------------------------------------
 def read_file(file_name)
   name_prices_array = []
-
-
   CSV.foreach("products.csv", headers: true) do |row|
     row_hash = { "Name" => row["name"], "SKU" => row["SKU"], "Retail Price" => row["retail_price"].to_f}
     name_prices_array << row_hash
   end
-
-  puts "Array: #{name_prices_array}"
+  #puts "Array: #{name_prices_array}"
   return name_prices_array
 end
-#read_file("products.csv")
+#read_file
+$prices_array = read_file("products.csv")
 
 
 #displays items to purchase from a CSV------------------------------------------------------------------------------------------------
-def displayTransactionOptions(file_name)
-  prices_hash = read_file(file_name)
+def displayTransactionOptions
   num_items = 1
 
-  prices_hash.each do |name, price|
-    puts "#{num_items}) Add item - $#{sprintf("%.2f", price)} - #{name}"
+  $prices_array.each do |nested_hash|
+    puts "#{num_items}) Add item - $#{sprintf("%.2f", nested_hash["Retail Price"])} - #{nested_hash["Name"]}"
     num_items += 1
-  end #end hash each loop
+  end #end array of hashes each loop
   puts "4) Complete Sale"
 end
-#displayTransactionOptions("products.csv")
+#displayTransactionOptions
+
 
 
 #select, quantity, subtotal------------------------------------------------------------------------------------------------
-def userInput(file_name)
-  prices_hash = read_file(file_name)
-  puts "Prices hash: #{prices_hash}"
+def userInput
+  puts "Prices array: #{$prices_array}"
 
   puts "Make a selection: "
   user_selection = gets.chomp.to_i
@@ -54,17 +52,19 @@ def userInput(file_name)
   puts "Selection: #{user_selection}, Quantity: #{user_quantity}, Subtotal: #{subtotal}"
 
 end
-#userInput("products.csv")
+#userInput
+
+
 
 #entire transaction using all other methods----------------------------------------------------------------------------------------
-def transaction(file_name)
+def transaction
   puts "Welcome to Jacinda's coffee emporium!"
   puts nil
-  displayTransactionOptions(file_name)
+  displayTransactionOptions
   puts nil
 
 
 
 end
-#transaction("products.csv")
+#transaction
 
