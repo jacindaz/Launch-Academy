d8dc7901fd1c450977fd1cd1fe7f619d0d4aeb5a
Jacinda Zhong <jacindaz@gmail.com>
2014-05-18 22:38:11 -0400

    Create average, max, min methods
    using sort_swap_array method, created methods that calculates the
    average, max, and min of a given array

--- systemscheck/systemscheck1/systemscheck1_v2.rb
+++ systemscheck/systemscheck1/systemscheck1_v2.rb
@@ -5,13 +5,20 @@ small_list = [85, 65, 100, 75, 20]
 
 
 
-
 def average(array)
   length = array.length
   sum = 0
 
-  return (sum/length.to_f)
+  array.each do |num|
+    sum += num
+  end
+
+  average = sum / (length.to_f)
+  #puts "Average: #{average}"
+
+  return average
 end
+#average(scores)
 
 
 def max(array)
@@ -38,8 +45,8 @@ def sort_swap_array(array)
       save_index1 = num1_index
       save_index2 = num2_index
       while (array[num1_index] < array[num1_index - 1]) && (num1_index > 0)
-        puts "index1: #{num1_index}, index2: #{num2_index}, array: #{array}"
-        puts "inner while, index1: #{num1_index - 1}, index2: #{num1_index}"
+        #puts "index1: #{num1_index}, index2: #{num2_index}, array: #{array}"
+        #puts "inner while, index1: #{num1_index - 1}, index2: #{num1_index}"
         save_num1 = array[num1_index - 1]
         array[num1_index - 1] = array[num1_index]
         array[num1_index] = save_num1
@@ -49,15 +56,26 @@ def sort_swap_array(array)
       num1_index = save_index1
       num2_index = save_index2
     end
-      puts "index1: #{num1_index}, index2: #{num2_index}, array: #{array}"
-      puts nil
+      #puts "index1: #{num1_index}, index2: #{num2_index}, array: #{array}"
+      #puts nil
 
       num1_index += 1
       num2_index += 1
 
   end
-
+  #puts "Final sorted array: #{array}"
+  return array
 end
 
+#sort_swap_array(scores)
 
-sort_swap_array(small_list)
+
+def avg_max_min(array)
+  average = average(array)
+  max = max(array)
+  min = min(array)
+
+  puts "For array: #{array}"
+  puts "Average: #{average}, Min: #{min}, Max: #{max}"
+end
+avg_max_min(scores)
