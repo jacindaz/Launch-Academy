6b3d5404d05f5a0d1b3930a4a4992a57e4d07b16
Jacinda Zhong <jacindaz@gmail.com>
2014-05-20 13:12:49 -0400

    Create loop for <li> list of tasks
     - use an each loop to loop through array of tasks
     - embed <li> tag

--- sinatra/webservers_http/server.rb
+++ sinatra/webservers_http/server.rb
@@ -1,3 +1,33 @@
 require 'sinatra'
+require 'rubygems'
+
 
 set :public_folder, File.dirname(__FILE__) + '/public'
+
+  tasks = ['pay bills', 'buy milk', 'learn Ruby', 'take over the world']
+
+  html = '''
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>Basic HTML Page</title>
+      <link rel="stylesheet" href="home.css" />
+    </head>
+
+    <body>
+      <h1>TODO list</h1>
+
+      <ul>
+  '''
+
+  tasks.each do |task|
+    html += "<li>#{task}</li>"
+  end
+
+  html += '''
+      </ul>
+    </body>
+  </html>
+  '''
+end
+
