e155bac7d2d8930c752a1728f3b65ef8921782b4
Jacinda Zhong <jacindaz@gmail.com>
2014-05-20 09:02:10 -0400

    Not working - merge_sort method to "merge" 2-element sorted items
    merge_sort method
     1) while loop, while the last element in array isn't nil
     2) (need to delete this) if an element is nil, skip
     3) if num1 > num2, add num2 to merged array
        if num2 > num1, add num1 to merged array
     4) in the non-merged array, the num added changed to nil
     5) because the value is now nil, the index for num added to merged
        array is incremented by 1
     6) if both num1 and num2 are nil, increment the indexes by 1

--- systemscheck/systemscheck1/merge_sort.rb
+++ systemscheck/systemscheck1/merge_sort.rb
@@ -1,16 +1,44 @@
-require 'pry'
+#require 'pry'
 scores = [75, 100, 85, 65, 84, 87, 95]
 reverse = [6,5,4,3,2,1]
 
 
 
 def merge_sort(array)
+  original_array = array
+  new_array = []
 
+  index1 = 0
+  index2 = 2
+  merged_array_index = 0
 
+  while (array.last != nil)
+    if (array[index1] == nil) || (array[index2] == nil)
+      next
+    end
+    puts "Merged: #{new_array}, Original: #{array}"
+    puts "Index1: #{index1}, Index2: #{index2}, Num1: #{array[index1]}, Num2: #{array[index2]}"
+    puts nil
+    if array[index2] < array[index1]
+      new_array[merged_array_index] = array[index2]
+      array[index2] = nil
+      index2 += 1
+    elsif array[index1] < array[index2]
+      new_array[merged_array_index] = array[index1]
+      array[index1] = nil
+      index1 += 1
+    elsif (array[index1] == nil) && (array[index2] == nil)
+      index1 += 1
+      index2 += 1
+    end
+    merged_array_index += 1
+  end #end while loop
+
+  return new_array
 
 end
 
-merge_sort(scores)
+merge_sort([5, 6, 3, 4, 1, 2, 7, 8])
 
 
 def merge_sort_2element(array)
@@ -27,4 +55,4 @@ def merge_sort_2element(array)
   end
 end
 #merge_sort_2element(scores)
-merge_sort_2element(reverse)
+#merge_sort_2element(reverse)
